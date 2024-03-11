1bb162cb4bac1feb0b136517b04ffbac9fda5088
being-devahmad <devahmad41@gmail.com>
2024-03-12 01:19:20 +0500

    Update Checkout.jsx

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -124,7 +124,7 @@ const Checkout = () => {
                                     </div>
                                     <div class="flex items-start mb-5">
                                         <div class="flex items-center h-5">
-                                            <input id="check" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                            <input id="check" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50" required />
                                         </div>
                                         <label for="check" class="ms-2 text-sm font-medium text-gray-900">Check me out</label>
                                     </div>
